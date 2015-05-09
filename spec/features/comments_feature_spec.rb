7faa21467cc35f6a6dc211341b6e625532047bcc
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-05-09 21:34:03 +0100

    allows users to write comments on pictures

---
+++ spec/features/comments_feature_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+feature 'writing comments' do
+  before{Picture.create name: 'photo'}
+
+  scenario 'allows users to write comments on pictures' do
+    visit '/pictures'
+    click_link 'Add a comment'
+    fill_in 'Thoughts', with: 'whatever'
+    click_button 'Leave Comment'
+    expect(page).to have_content 'whatever'
+    expect(current_path).to eq '/pictures'
+  end
+end
