1097d17c0ae76a2754af9e7a69c210be64f70227
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-05-09 18:29:20 +0100

    allow a user to completely change a picture

--- spec/features/picture_feature_spec.rb
+++ spec/features/picture_feature_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-feature 'pictures' do
-  context 'no picture have been added yet' do
+describe 'Instagram s main page' do
+  feature 'no picture have been added yet' do
     scenario 'should display a prompt to add a picture' do
       visit '/pictures'
       expect(page).to have_content 'No pictures yet!'
@@ -9,43 +9,45 @@ feature 'pictures' do
     end
   end
 
-  context 'pictures have been added' do
-    before do
-    Picture.create(name: 'photo')
+  feature 'pictures have been added' do
+    let!(:photo){Picture.create(name:'photo')}
     # I wanted to do pic with url instead of a name at first.
     # didnt work. Need to ask Spike for advice.
     # body = [{
     #       :url => 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/7/000/1ef/1bf/1fab8e9.jpg' }
     #       }]
-    end
 
-    scenario 'display pictures names(for now)' do
+
+    scenario 'displays each picture\'s name on main page' do
       visit '/pictures'
       expect(page).to have_content('photo')
       # expect(page).to have_selector("img[src$='https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/7/000/1ef/1bf/1fab8e9.jpg']")
       expect(page).not_to have_content('No pictures yet!')
     end
-  end
 
-  context 'Adding pictures' do
-    scenario 'asks user to attach a picture (with its name, url later), then displays it' do
+    scenario 'allows a user to add a new picture that is then displayed instantly' do
       visit '/pictures'
       click_link 'Add a picture'
-      fill_in 'Name', with: 'photo'
+      fill_in 'Name', with: 'photo2'
       click_button 'Create Picture'
-      expect(page).to have_content 'photo'
+      expect(page).to have_content 'photo2'
       expect(current_path).to eq '/pictures'
     end
-  end
-
-  context 'Checking a picture\'s pages' do
-    let!(:photo){Picture.create(name:'photo')}
     
-    scenario 'let a user view a picture\'s page' do
+    scenario 'a user can view a picture\'s own page by clicking on it' do
       visit '/pictures'
       click_link 'photo'
       expect(page).to have_content 'photo'
       expect(current_path).to eq "/pictures/#{photo.id}"
+    end
+    
+    scenario 'allow a user to completely change a picture' do
+      visit '/pictures'
+      click_link 'Edit photo'
+      fill_in 'Name', with: 'newphoto'
+      click_button 'Update Picture'
+      expect(page).to have_content 'newphoto'
+      expect(current_path).to eq '/pictures'
     end
   end
 end
